7258cfc7fdac7afb7534a8c0b637b21e72e75fed
88KMA <maximkozlov88@gmail.com>
2024-09-22 23:42:11 +0300

    Add files via upload

---
+++ module_1_5_item_4.py
@@ -0,0 +1,6 @@
+mutable_list = ['apple', 'coconut', 'peach', 'banana']
+mutable_list[1] = 'melon'
+mutable_list.append('lemon')
+mutable_list.extend('papaya')
+mutable_list.remove('banana')
+print(mutable_list)
